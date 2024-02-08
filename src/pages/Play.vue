771dd753d1887ea2b8805c6de3a61c58b8694d2b
Thu Htet Naing <demoinerl2h@gmail.com>
2024-02-09 01:38:11 +0630

    update vue

--- src/pages/Play.vue
+++ src/pages/Play.vue
@@ -5,7 +5,7 @@ import PlayerScore from '../components/PlayerScore/index.vue';
 </script>
 <template>
     <div class="container">
-        <h1 style="text-align: center;">Tic Tac Toe</h1>
+        <h1 style="text-align: center;">Tic Tac Toe, Hi Jojo</h1>
         <PlayerScore style="margin-bottom: 0.7rem;" />
         <ButtonsComponents />
     </div>
